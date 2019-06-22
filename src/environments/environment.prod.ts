2f1ccd7c0dfbeeed882d195de4e57572f515f874
Abhishek kumar <38450711+7asterisk@users.noreply.github.com>
2019-06-22 23:30:48 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAzIvsKHBbleGeWvjAgbJsNFv_XM6H7TII',
-    authDomain: 'niralasconsultancy.firebaseapp.com',
-    databaseURL: 'https://niralasconsultancy.firebaseio.com',
-    projectId: 'niralasconsultancy',
-    storageBucket: 'niralasconsultancy.appspot.com',
-    messagingSenderId: '1037300197578',
-    appId: '1:1037300197578:web:d82035984e635f8a'
+    
   }
 };
